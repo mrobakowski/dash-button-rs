465b4a8ec3c562312c8460ce59ff1551d1c3c28f
mrobakowski <mrobakowski@networkedassets.org>
2017-03-21 17:11:25 +0100

    Formatting

--- examples/find-dash-button.rs
+++ examples/find-dash-button.rs
@@ -22,9 +22,8 @@ fn run() -> Result<()> {
     let device = choose_device()?;
 
     println!("\n🔍 Searching for Amazon Dash Button...");
-    let mac = AmazonDashButton::discover_on(device.name.as_str())?;
+    let mac = AmazonDashButton::discover_on(device.name.as_str())?; // for some reason Device isn't Clone, but this works
     println!("Discovered Amazon Dash Button with mac {}\n\nListening for presses...", mac);
-
 
     use chrono::prelude::*;
     for DashButtonEvent { time, .. } in AmazonDashButton::from_mac(mac)?.listen_on(device).events()? {
@@ -37,21 +36,22 @@ fn run() -> Result<()> {
 
 fn choose_device() -> Result<Device> {
     let devices = Device::list()?;
+
     println!("Found devices:");
     for (i, device) in devices.iter().enumerate() {
         println!("{}) {}\t{}", i + 1, device.name,
                  device.desc.as_ref().map(|x| x.as_str()).unwrap_or("<no description>"));
     }
+
     print!("Device to use [1]: ");
     io::stdout().flush()?;
+
     let mut choice = String::new();
     io::stdin().read_line(&mut choice)?;
+
     let choice = choice.trim();
-    let choice: usize = if choice.is_empty() {
-        1
-    } else {
-        choice.parse()?
-    } - 1;
+    let choice: usize = if choice.is_empty() { 1 } else { choice.parse()? };
+    let choice = choice - 1;
 
     Ok(devices.into_iter().nth(choice).ok_or("index out of bounds")?)
 }
